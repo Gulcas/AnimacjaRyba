2ad31cc0b97eb14469dfe9e80d1363f89d22451c
rgranoszewsk <rafal.granoszewski@zalando.pl>
2023-11-23 13:24:18 +0100

    1

--- app/src/main/java/com/rafal/animacjaryba/MainActivity.java
+++ app/src/main/java/com/rafal/animacjaryba/MainActivity.java
@@ -3,6 +3,10 @@ package com.rafal.animacjaryba;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void obracaj(View v) {
+        Animation a = AnimationUtils.loadAnimation(this, R.anim.obrot);
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+        iv.startAnimation(a);
+    }
+    public void zanikaj(View v) {
+        Animation a = AnimationUtils.loadAnimation(this, R.anim.zanik);
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+        iv.startAnimation(a);
+    }
+    public void przesuwaj(View v) {
+        Animation a = AnimationUtils.loadAnimation(this, R.anim.przesuw);
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+        iv.startAnimation(a);
+    }
+    public void skaluj(View v) {
+        Animation a = AnimationUtils.loadAnimation(this, R.anim.skalowanie);
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+        iv.startAnimation(a);
+    }
+    public void stosuj_wszystko(View v) {
+        Animation a = AnimationUtils.loadAnimation(this, R.anim.wszytko);
+        ImageView iv = (ImageView) findViewById(R.id.imageView);
+        iv.startAnimation(a);
     }
 }
